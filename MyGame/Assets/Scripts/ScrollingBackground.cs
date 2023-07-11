c24764f4579ce921d8b8c7ed9260606fbcd8ea31
ardailgazz <ilgazarda8@gmail.com>
2023-07-11 16:49:16 +0300

    Background
    Background tamamlandı

---
+++ MyGame/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    [SerializeField] float loopSpeed;
+    [SerializeField] Transform backgroundTransform;
+    private float x, y, z;
+    private void Update()
+    {
+        x = backgroundTransform.position.x - loopSpeed * Time.deltaTime; //her frame arası ne kadar zaman geçtiğini hesaplıyor
+        y = backgroundTransform.position.y;
+        z = backgroundTransform.position.z;
+
+        Vector3 newPosition = new Vector3(x, y, z); //yeni pozisyonu Vektör3'e tanımladım
+
+        backgroundTransform.position = newPosition;
+
+        //-17.73
+        if (backgroundTransform.position.x < -17.73)
+        {
+            Vector3 startPosition = new Vector3(0, 0, 0);
+
+            backgroundTransform.position = startPosition;
+        } 
+    }
+    
+    
+}
